0f69966fa75b95cc038b6502f44b7d80d757d247
Kevin <mrhallonline@gmail.com>
2019-10-28 12:17:18 -0500

    add image to body and center

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # i_am_rich
 
-A new Flutter project.
+Building tutorial app for Flutter Boot Camp course.
 
 ## Getting Started
 
--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey[200],
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           centerTitle: true,
           backgroundColor: Colors.blueGrey[900],
           title: Text('I am Rich'),
+          
         ),
-      ),
-    );
+        body: 
+        Center(
+          child: Image(
+                  image: NetworkImage('https://cdn.arstechnica.net/wp-content/uploads/2016/02/5718897981_10faa45ac3_b-640x624.jpg'),
+          ),
+        ),
+              
+            ),
+      );
   }
 }
