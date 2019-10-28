21ab4d3cdfd352d0f2c8ffd6566a2c4f7466abe5
Kevin <mrhallonline@gmail.com>
2019-10-28 11:40:40 -0500

    appBar and background color

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Center(child: Text('Hello World'))));
+void main() => runApp(MyApp());
 
-
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[200],
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[900],
+          title: Text('I am Rich'),
+        ),
+      ),
+    );
+  }
+}
